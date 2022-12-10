eec961a22188c8480aae9d5789090413ac607688
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2022-12-10 16:13:31 +0700

    feat: add forms component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import FormFile from "./components/form/TwFile.vue";
+import TwFile from "./components/form/TwFile.vue";
+import TwInput from "./components/form/TwInput.vue";
+import TwMultiSelect from "./components/form/TwMultiSelect.vue";
+import TwSelect from "./components/form/TwSelect.vue";
+import TwToggle from "./components/form/TwToggle.vue";
 
 const fileModel = ref([]);
+const selectExample = ref();
+const multiSelectExample = ref([]);
+const inputExample = ref();
+const toggleExample = ref();
+
+const selectionList = [
+  {
+    label: "test",
+    value: "test",
+  },
+  {
+    label: "test2",
+    value: "test2",
+  },
+  {
+    label: "test3",
+    value: "test3",
+  },
+];
 </script>
 
 <template>
@@ -10,11 +33,56 @@ const fileModel = ref([]);
     <div class="container">
       <div class="grid grid-cols-12">
         <div class="col-span-12">
-          <FormFile v-model="fileModel" />
-        </div>
-        <div class="col-span-12">
+          <TwFile v-model="fileModel" />
           <div class="">Result:</div>
           <pre>{{ fileModel }}</pre>
+        </div>
+        <div class="col-span-12 my-2">
+          <hr />
+        </div>
+        <div class="col-span-12">
+          <TwInput
+            v-model="inputExample"
+            label="Input"
+            placeholder="Input Field"
+            type="text"
+          />
+          <div class="">Result:</div>
+          <pre>{{ inputExample }}</pre>
+        </div>
+        <div class="col-span-12 my-2">
+          <hr />
+        </div>
+        <div class="col-span-12">
+          <TwSelect
+            v-model="selectExample"
+            :items="selectionList"
+            label="Single Select"
+            placeholder="Choose select"
+          />
+          <div class="">Result:</div>
+          <pre>{{ selectExample }}</pre>
+        </div>
+        <div class="col-span-12 my-2">
+          <hr />
+        </div>
+        <div class="col-span-12">
+          <TwMultiSelect
+            v-model="multiSelectExample"
+            :items="selectionList"
+            label="Multi Select"
+            placeholder="Choose select"
+          />
+          <div class="">Result:</div>
+          <pre>{{ multiSelectExample }}</pre>
+        </div>
+        <div class="col-span-12 my-2">
+          <hr />
+        </div>
+        <div class="col-span-12">
+          <TwToggle v-model="toggleExample" label="Toggle" />
+          <div class="">Result:</div>
+          <pre>{{ toggleExample }}</pre>
         </div>
       </div>
     </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,16 @@
+import "../assets/build.css";
 import TwFile from "./form/TwFile.vue";
-import "../assets/main.css";
+import TwMultiSelect from "./form/TwMultiSelect.vue";
+import TwSelect from "./form/TwSelect.vue";
+import TwTextarea from "./form/TwTextarea.vue";
+import TwToggle from "./form/TwToggle.vue";
+import TwInput from "./form/TwInput.vue";
 
-export default { TwFile };
+export default {
+  TwFile,
+  TwInput,
+  TwMultiSelect,
+  TwSelect,
+  TwTextarea,
+  TwToggle,
+};
