b8ae9c5a8fa8c46c5ed927e7c3a92d550aaeebbe
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2022-12-27 20:45:07 +0700

    chore: rollback config ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,24 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-let build = {
-  cssCodeSplit: false,
-  lib: {
-    entry: "./src/build.ts",
-    formats: ["es", "cjs"],
-    name: "Vue3Tailwind",
-    fileName: "vue3-tailwind",
-  },
-  rollupOptions: {
-    external: ["vue"],
-    output: {
-      globals: {
-        vue: "Vue",
+export default defineConfig({
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: "./src/build.ts",
+      formats: ["es", "cjs"],
+      name: "Vue3Tailwind",
+      fileName: "vue3-tailwind",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
       },
     },
   },
-};
-if (process.env.BUILD_MODE === "VERCEL") {
-  build = {};
-}
-console.log(build)
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: build,
   plugins: [vue()],
   resolve: {
     alias: {
