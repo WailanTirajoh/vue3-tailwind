6a348aaa8fb9b6b120ffc72c96d3e74d1a967334
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2023-01-14 19:58:13 +0700

    chore: modify build.ts

--- src/build.ts
+++ src/build.ts
@@ -1,9 +1,5 @@
-import "./assets/build/build.css";
 import type { App, Directive } from "vue";
-import Ripple from "./directives/ripple";
-export * from "./type";
-import * as components from "./components";
-export {
+import {
   TwFile,
   TwInput,
   TwMultiSelect,
@@ -29,7 +25,91 @@ export {
   TwTabBody,
 } from "./components";
 
-function install(app: App) {
+import Ripple from "./directives/ripple";
+import { useToast } from "./composables/toast";
+import { useForm } from "./composables/form";
+import { useDialog } from "./composables/dialog";
+import { useBreakpoints } from "./composables/breakpoints";
+import { useTabs } from "./composables/tabs";
+import "./assets/build/build.css";
+
+export type {
+  ButtonVariant,
+  ButtonIconPosition,
+  ButtonTextPosition,
+  DatatableColumn,
+  DatatableData,
+  DatatableSetting,
+  DropdownAlign,
+  DropdownItemValue,
+  DropdownItem,
+  RadioOption,
+  ToastType,
+  ToastDeafult,
+  ModalBackdrop,
+  OffcanvasPosition,
+  Tab,
+  TabChildren,
+  TabRefs,
+} from "./type";
+
+// Export components
+export {
+  TwFile,
+  TwInput,
+  TwMultiSelect,
+  TwSelect,
+  TwTextarea,
+  TwToggle,
+  TwButton,
+  TwTab,
+  TwOffcanvas,
+  TwModal,
+  TwDatatableClient,
+  TwDatatableServer,
+  TwFeather,
+  TwToast,
+  TwDropdownMenu,
+  TwAccordion,
+  TwForm,
+  TwErrorMessage,
+  TwDialog,
+  TwRadio,
+  TwTabWrapper,
+  TwTabNavigator,
+  TwTabBody,
+};
+
+// Export composables
+export { useToast, useForm, useDialog, useBreakpoints, useTabs };
+
+export const Vue3Tailwind = function install(app: App) {
+  const components = {
+    TwFile,
+    TwInput,
+    TwMultiSelect,
+    TwSelect,
+    TwTextarea,
+    TwToggle,
+    TwButton,
+    TwTab,
+    TwOffcanvas,
+    TwModal,
+    TwDatatableClient,
+    TwDatatableServer,
+    TwFeather,
+    TwToast,
+    TwDropdownMenu,
+    TwAccordion,
+    TwForm,
+    TwErrorMessage,
+    TwDialog,
+    TwRadio,
+    TwTabWrapper,
+    TwTabNavigator,
+    TwTabBody,
+  };
+
   Object.values(components).forEach((component) => {
     app.component(component.name, component);
   });
@@ -37,6 +117,4 @@ function install(app: App) {
   Ripple.color = "rgba(255, 255, 255, 0.35)";
   Ripple.zIndex = "55";
   app.directive("ripple", Ripple as Directive<any, any>);
-}
-
-export const Vue3Tailwind = install;
+};
